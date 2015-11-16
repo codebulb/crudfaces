c69064088eec949ae4c4703fe37c5c7efdeeab16
codebulb <codebulb@users.noreply.github.com>
2015-11-16 18:45:13 +0300

    License update

--- src/main/resources/META-INF/resources/crudfaces/crudfaces.css
+++ src/main/resources/META-INF/resources/crudfaces/crudfaces.css
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 CrudFaces.
+ * Copyright 2015 CrudFaces / Nicolas Hofstetter (codebulb.ch).
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
